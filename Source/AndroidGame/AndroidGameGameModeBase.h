9d65ac209b5a5a8234109a92147bccbe65613d34
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-12-13 16:20:13 +0800

    UE5.2安卓学习分支 视频地址：https://www.youtube.com/watch?v=5yeLsgvpdwU&list=PLMCnTPAjnxO-x0jDRK4xC-V3hZ4nBmlRZ&index=2

---
+++ Source/AndroidGame/AndroidGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndroidGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANDROIDGAME_API AAndroidGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
